9b1a6ca7bb54970b88f8d2e65136fba7ba4e4c95
Vytenis Bivainis <monrealis@gmail.com>
2015-01-28 21:23:24 +0200

    Add tests to verify "create = true" parameter.

--- java7/src/test/java/java7/io/ZipTest.java
+++ java7/src/test/java/java7/io/ZipTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
@@ -14,6 +15,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipError;
 import java.util.zip.ZipOutputStream;
 
 import static org.junit.Assert.assertEquals;
@@ -21,13 +23,23 @@ import static org.junit.Assert.assertTrue;
 
 public class ZipTest {
     private Path temp;
+    private List<Path> deleteAfterTest = new ArrayList<>();
     private Map<String, String> env = new HashMap<>();
     private FileSystem fileSystem;
 
     @Before
     public void before() throws IOException {
         temp = createEmptyZip();
-        fileSystem = FileSystems.newFileSystem(URI.create("jar:file:" + temp), env, null);
+        fileSystem = createZipFileSystem();
+    }
+
+    private FileSystem createZipFileSystem() throws IOException {
+        URI uri = URI.create("jar:file:" + temp);
+        return createZipFileSystem(uri);
+    }
+
+    private FileSystem createZipFileSystem(URI uri) throws IOException {
+        return FileSystems.newFileSystem(uri, env, null);
     }
 
     private Path createEmptyZip() throws IOException {
@@ -42,11 +54,13 @@ public class ZipTest {
 
     @After
     public void after() {
-        assertTrue(temp.toFile().delete());
+        for (Path p : deleteAfterTest) {
+            assertTrue(p.toFile().delete());
+        }
     }
 
     @Test
-    public void rootDirectoryExists() throws IOException {
+    public void inNewZip_rootDirectoryExists() throws IOException {
         assertEquals("/", getRoot().toString());
     }
 
@@ -66,7 +80,7 @@ public class ZipTest {
     }
 
     @Test
-    public void dirGetsCreated() throws IOException {
+    public void dirGetsAdded() throws IOException {
         assertEmpty();
         Files.createDirectory(fileSystem.getPath("/dir.txt"));
         assertEquals("/dir.txt/", list().get(0).toString());
@@ -79,5 +93,22 @@ public class ZipTest {
             r.add(p);
         }
         return r;
+    }
+
+    @Test
+    public void ifZipFileIsMissing_newZipGetsCreated() throws IOException {
+        temp = new File(temp.toString() + ".zip").toPath();
+        deleteAfterTest.add(temp);
+        env.put("create", "true");
+        createZipFileSystem();
+    }
+
+    @Test(expected = ZipError.class)
+    public void ifZipFileIsEmpty_newZipDoesNotGetCreated() throws IOException {
+        fileSystem.close();
+        Files.delete(temp);
+        Files.createFile(temp);
+        env.put("create", "true");
+        createZipFileSystem();
     }
 }
