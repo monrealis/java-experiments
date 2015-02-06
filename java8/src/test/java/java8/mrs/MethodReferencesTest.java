9c95098ef99ef2781168844ac362addaed14ef1e
Vytenis Bivainis <monrealis@gmail.com>
2015-02-07 00:40:40 +0200

    Add MethodReferencesTest.

---
+++ java8/src/test/java/java8/mrs/MethodReferencesTest.java
@@ -0,0 +1,39 @@
+package java8.mrs;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.asList;
+import static java.util.Comparator.comparing;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class MethodReferencesTest {
+    private List<Integer> ints = IntStream.rangeClosed(1, 10).boxed().collect(Collectors.toList());
+
+    @Before
+    public void before() {
+        Collections.shuffle(ints);
+    }
+
+    @Test
+    public void usingMethodReferences_sorted() {
+        ints.sort(comparing(Object::toString));
+        assertSorted();
+    }
+
+    @Test
+    public void usingLambdas_sorted() {
+        ints.sort(comparing(a -> a.toString()));
+        assertSorted();
+    }
+
+    private void assertSorted() {
+        assertThat(ints.subList(0, 3), equalTo(asList(1, 10, 2)));
+    }
+}
