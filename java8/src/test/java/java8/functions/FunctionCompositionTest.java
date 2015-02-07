f615673247b3422be45935a7c3741ac42961af90
Vytenis Bivainis <monrealis@gmail.com>
2015-02-07 22:39:08 +0200

    Add tests for Function and Predicate defaults methods.

---
+++ java8/src/test/java/java8/functions/FunctionCompositionTest.java
@@ -0,0 +1,22 @@
+package java8.functions;
+
+import org.junit.Test;
+
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
+
+public class FunctionCompositionTest {
+    Function<Integer, Integer> addOne = i -> i + 1;
+    Function<Integer, Integer> timesTwo = i -> i * 2;
+
+    @Test
+    public void andThen_passes() {
+        assertEquals(4, addOne.andThen(timesTwo).apply(1).intValue());
+    }
+
+    @Test
+    public void compose_passes() {
+        assertEquals(3, addOne.compose(timesTwo).apply(1).intValue());
+    }
+}
---
+++ java8/src/test/java/java8/predicates/PredicateCompositionTest.java
@@ -0,0 +1,22 @@
+package java8.predicates;
+
+import org.junit.Test;
+
+import java.util.function.Predicate;
+
+import static org.junit.Assert.assertTrue;
+
+public class PredicateCompositionTest {
+    Predicate<Integer> truePredicate = (i) -> true;
+    Predicate<Integer> falsePredicate = (i) -> false;
+
+    @Test
+    public void andAndNot_pass() {
+        assertTrue(truePredicate.and(falsePredicate.negate()).test(null));
+    }
+
+    @Test
+    public void or_passes() {
+        assertTrue(truePredicate.or(falsePredicate).test(null));
+    }
+}
