2c743b29af02a1c0f60ca6292f14f76e50b8ef72
Vytenis Bivainis <monrealis@gmail.com>
2015-02-23 20:53:26 +0200

    Add ambiguous call.

--- java8/src/test/java/java8/lambdas/LambdasTest.java
+++ java8/src/test/java/java8/lambdas/LambdasTest.java
@@ -8,11 +8,30 @@ import static org.junit.Assert.assertEquals;
 
 public class LambdasTest {
     @Test
-    public void supplier_works() {
+    public void supplier_passes() {
         assertEquals("x", echo(() -> "x"));
     }
 
     private String echo(Supplier<String> s) {
         return s.get();
+    }
+
+    @Test
+    public void ambiguousCalls_resolvedByCasting() {
+        consume((Runnable) System.out::println);
+        consume((OtherTypeOfRunnable) System.out::println);
+    }
+
+    private void consume(OtherTypeOfRunnable c) {
+        c.run();
+    }
+
+    private void consume(Runnable r) {
+        r.run();
+    }
+
+    @FunctionalInterface
+    public static interface OtherTypeOfRunnable {
+        void run();
     }
 }
