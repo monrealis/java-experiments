2d7eaacbd79e2dd63a1139aad46a636d3a5e0513
Vytenis Bivainis <monrealis@gmail.com>
2015-02-26 23:56:40 +0200

    Add TableFixture.

---
+++ fitnesse/src/main/java/eu/vytenis/fitnesse/table/SumTableFixture.java
@@ -0,0 +1,45 @@
+package eu.vytenis.fitnesse.table;
+
+import fitnesse.fixtures.TableFixture;
+
+public class SumTableFixture extends TableFixture {
+    int sum = 0;
+
+    @Override
+    protected void doStaticTable(int rows) {
+        for (int i = 0; i < rows; ++i)
+            handleRow(i);
+    }
+
+    private void handleRow(int i) {
+        if (getText(i, 0).equals("addOne"))
+            handleAddOne(i);
+        else if (getText(i, 0).equals("addTwo"))
+            handleAddTwo(i);
+        else if (getText(i, 0).equals("checkSum"))
+            handleCheckSum(i);
+    }
+
+    private void handleAddOne(int i) {
+        addOne(getInt(i, 1));
+    }
+
+    private void handleAddTwo(int i) {
+        addTwo(getInt(i, 1), getInt(i, 2));
+    }
+
+    private void addOne(int number) {
+        sum += number;
+    }
+
+    private void addTwo(int number1, int number2) {
+        sum += number1 + number2;
+    }
+
+    private void handleCheckSum(int i) {
+        if (sum == getInt(i, 1))
+            right(i, 1);
+        else
+            wrong(i, 1, String.valueOf(sum));
+    }
+}
---
+++ fitnesse/src/test/java/eu/vytenis/fitnesse/table/SumTableFixtureTest.java
@@ -0,0 +1,11 @@
+package eu.vytenis.fitnesse.table;
+
+import fitnesse.junit.FitNesseRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(FitNesseRunner.class)
+@FitNesseRunner.Suite("Tests.SumTableFixtureTest")
+@FitNesseRunner.FitnesseDir(".")
+@FitNesseRunner.OutputDir("target/fitnesse-results")
+public class SumTableFixtureTest {
+}
