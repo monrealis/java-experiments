85f0f6016e9b4bc1f3d0e2536b29811c8c76b101
Vytenis Bivainis <monrealis@gmail.com>
2015-02-28 00:33:28 +0200

    Add sequence fixture.

---
+++ fitnesse/src/main/java/eu/vytenis/fitnesse/sequence/SumSequenceFixture.java
@@ -0,0 +1,31 @@
+package eu.vytenis.fitnesse.sequence;
+
+import fitlibrary.SequenceFixture;
+
+public class SumSequenceFixture extends SequenceFixture {
+    int sum = 0;
+
+    public void addOne(int number) {
+        sum += number;
+    }
+
+    public void addTwo(int number1, int number2) {
+        sum += number1 + number2;
+    }
+
+    public void addLetterCounts(String s1, String s2) {
+        addTwo(s1.length(), s2.length());
+    }
+
+    public void twice() {
+        sum *= 2;
+    }
+
+    public int sum() {
+        return sum;
+    }
+
+    public int sumPlus(int number) {
+        return sum + number;
+    }
+}
---
+++ fitnesse/src/test/java/eu/vytenis/fitnesse/sequence/SumSequenceFixtureTest.java
@@ -0,0 +1,12 @@
+package eu.vytenis.fitnesse.sequence;
+
+
+import fitnesse.junit.FitNesseRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(FitNesseRunner.class)
+@FitNesseRunner.Suite("Tests.SumSequenceFixtureTest")
+@FitNesseRunner.FitnesseDir(".")
+@FitNesseRunner.OutputDir("target/fitnesse-results")
+public class SumSequenceFixtureTest {
+}
