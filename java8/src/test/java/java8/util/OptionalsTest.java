87d5134f1fce938562f6b69743bfe18611b9c191
Vytenis Bivainis <monrealis@gmail.com>
2015-03-02 22:52:19 +0200

    Add more tests for optionals.

--- java8/src/test/java/java8/util/OptionalsTest.java
+++ java8/src/test/java/java8/util/OptionalsTest.java
@@ -3,9 +3,10 @@ package java8.util;
 import org.junit.Test;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static java.util.Optional.*;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 
 public class OptionalsTest {
     @Test(expected = NoSuchElementException.class)
@@ -27,5 +28,43 @@ public class OptionalsTest {
     @Test
     public void givenNull_ofNullable_returnsEmpty() {
         assertSame(ofNullable(null), empty());
+    }
+
+    @Test
+    public void givenTwoOptionals_join_returnsPresentOnlyIfBothPresent() {
+        Optional<String> x = of("x");
+        Optional<String> e = empty();
+        assertEquals("xx", join(x, x).get());
+        assertFalse(join(x, e).isPresent());
+        assertFalse(join(e, x).isPresent());
+        assertFalse(join(e, e).isPresent());
+    }
+
+    private Optional<String> join(Optional<String> first, Optional<String> second) {
+        return first.flatMap(s -> second.map(x -> s + x));
+    }
+
+    @Test
+    public void getName_returnsNameOrDefault() {
+        Person p = new Person();
+        assertEquals("-", getName(of(p)));
+        p.parent = p;
+        assertEquals("name", getName(of(p)));
+    }
+
+    private String getName(Optional<Person> o) {
+        return o.map(Person::getParent).map(Person::getParent).map(Person::getParent).map(Person::getName).orElse("-");
+    }
+
+    public class Person {
+        private Person parent;
+
+        private String getName() {
+            return "name";
+        }
+
+        public Person getParent() {
+            return parent;
+        }
     }
 }
