8cab1ec5abe74c6ed248e92d7cdf662de88b2b08
Vytenis Bivainis <monrealis@gmail.com>
2015-03-06 21:54:33 +0200

    Add thenCombine() and thenCompose() tests.

--- java8/src/test/java/java8/futures/CompletableFutureTest.java
+++ java8/src/test/java/java8/futures/CompletableFutureTest.java
@@ -8,19 +8,40 @@ import java.util.concurrent.ExecutionException;
 import static org.junit.Assert.assertEquals;
 
 public class CompletableFutureTest {
+    public static final Integer TWO = 2;
+    public static final Integer THREE = 3;
+    public static final Integer FOUR = 4;
+
     @Test
     public void get() throws ExecutionException, InterruptedException {
         CompletableFuture<Integer> cf = supplyTwoAsync();
-        assertEquals(new Integer(2), cf.get());
+        assertEquals(TWO, cf.get());
     }
 
     private CompletableFuture<Integer> supplyTwoAsync() {
         return CompletableFuture.supplyAsync(() -> 2);
     }
 
+
     @Test
     public void join() {
         CompletableFuture<Integer> cf = supplyTwoAsync();
-        assertEquals(new Integer(2), cf.join());
+        assertEquals(TWO, cf.join());
+    }
+
+    @Test
+    public void thenCombine() {
+        assertEquals(FOUR, supplyTwoAsync().thenCombine(supplyTwoAsync(), (a, b) -> a + b).join());
+        assertEquals(FOUR, supplyTwoAsync().thenCombineAsync(supplyTwoAsync(), (a, b) -> a + b).join());
+    }
+
+    @Test
+    public void thenCompose() {
+        assertEquals(THREE, supplyTwoAsync().thenCompose(this::addOneAsync).join());
+        assertEquals(THREE, supplyTwoAsync().thenComposeAsync(this::addOneAsync).join());
+    }
+
+    private CompletableFuture<Integer> addOneAsync(int number) {
+        return CompletableFuture.supplyAsync(() -> number + 1);
     }
 }
