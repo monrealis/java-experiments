f3a4dff4c7c09ca09e23ece20e18a9fe8400121a
Vytenis Bivainis <monrealis@gmail.com>
2015-04-03 22:59:23 +0300

    Add NumericLiteralsTest.

---
+++ java7/src/test/java/java7/numbers/NumericLiteralsTest.java
@@ -0,0 +1,12 @@
+package java7.numbers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumericLiteralsTest {
+    @Test
+    public void shouldUnderstandUnderscoresInNumericLiterals() {
+        assertEquals(1000, 10_0_0);
+    }
+}
