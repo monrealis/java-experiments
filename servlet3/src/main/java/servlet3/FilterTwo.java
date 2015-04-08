dc17817ef1ad30dd41022144773c387890fef285
Vytenis Bivainis <monrealis@gmail.com>
2015-04-08 22:50:09 +0300

    Add init parameter.

--- servlet3/src/main/java/servlet3/FilterTwo.java
+++ servlet3/src/main/java/servlet3/FilterTwo.java
@@ -2,17 +2,20 @@ package servlet3;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 import java.io.IOException;
 
-@WebFilter("/two")
+@WebFilter(value = "/two", initParams = @WebInitParam(name = "text", value = "Text"))
 public class FilterTwo implements Filter {
+    private FilterConfig config;
     @Override
     public void init(FilterConfig config) {
+        this.config = config;
     }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException {
-        resp.getWriter().write("Two");
+        resp.getWriter().write("Two. " + config.getInitParameter("text"));
     }
 
     @Override
