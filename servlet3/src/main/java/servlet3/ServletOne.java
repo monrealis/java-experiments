0e7126607e6fde32d578537f4a189c4c3d052092
Vytenis Bivainis <monrealis@gmail.com>
2015-04-11 21:37:33 +0300

    Add constraints.

--- servlet3/src/main/java/servlet3/ServletOne.java
+++ servlet3/src/main/java/servlet3/ServletOne.java
@@ -1,5 +1,8 @@
 package servlet3;
 
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.HttpMethodConstraint;
+import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/one")
+@ServletSecurity(value = @HttpConstraint(rolesAllowed = "One"), httpMethodConstraints = @HttpMethodConstraint(value = "POST", rolesAllowed = "One"))
 public class ServletOne extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
