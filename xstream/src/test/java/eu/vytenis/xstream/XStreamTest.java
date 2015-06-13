a67f8ad238c1103bfda45c25eb7c4054220ca550
Vytenis Bivainis <monrealis@gmail.com>
2015-06-13 14:19:56 +0300

    Add more tests.

--- xstream/src/test/java/eu/vytenis/xstream/XStreamTest.java
+++ xstream/src/test/java/eu/vytenis/xstream/XStreamTest.java
@@ -1,18 +1,39 @@
 package eu.vytenis.xstream;
 
 import com.thoughtworks.xstream.XStream;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonMap;
+
 public class XStreamTest {
     @Rule
     public TestName name = new TestName();
+    private XStream xs = new XStream();
+
+    @Before
+    public void before() {
+        xs.alias("xs", XStreamTest.class);
+    }
 
     @Test
-    public void run() {
-        XStream x = new XStream();
-        x.alias("xs", XStreamTest.class);
+    public void serializesThis() {
+        XStream x = xs;
+        xs = null;
         System.out.println(x.toXML(this));
+    }
+
+    @Test
+    public void serializesMaps() {
+        System.out.println(xs.toXML(emptyMap()));
+        System.out.println(xs.toXML(singletonMap("x", 1)));
+        System.out.println(xs.toXML(new HashMap<>()));
+        System.out.println(xs.toXML(new TreeMap<>()));
     }
 }
