f21cc837b87076618e39e5f8e5f9f5fb50b0546f
Vytenis Bivainis <monrealis@gmail.com>
2015-11-16 21:23:19 +0200

    Create NullEntry.

--- coding/src/main/java/coding/hashtables/Hash.java
+++ coding/src/main/java/coding/hashtables/Hash.java
@@ -4,13 +4,13 @@ public class Hash<K> {
     private Entry<K>[] buckets;
 
     public Hash(int numberOfBuckets) {
-        buckets = (Entry<K>[]) new Entry[numberOfBuckets];
+        buckets = (SimpleEntry<K>[]) new SimpleEntry[numberOfBuckets];
     }
 
     public boolean contains(K key) {
         Entry<K> entry = buckets[getBucketIndex(key)];
         while (entry != null) {
-            if (entry.getValue().equals(key))
+            if (entry.isValueEqualTo(key))
                 return true;
             entry = entry.getNext();
         }
@@ -20,7 +20,7 @@ public class Hash<K> {
     public void add(K key) {
         int index = getBucketIndex(key);
         Entry<K> first = buckets[index];
-        Entry<K> entry = new Entry<>(key);
+        SimpleEntry<K> entry = new SimpleEntry<>(key);
         if (first == null)
             buckets[index] = entry;
         else
@@ -32,7 +32,7 @@ public class Hash<K> {
         Entry<K> entry = buckets[index];
         Entry<K> previousEntry = null;
         while (entry != null) {
-            if (entry.getValue().equals(key))
+            if (entry.isValueEqualTo(key))
                 if (previousEntry != null)
                     previousEntry.setNext(entry.getNext());
                 else
@@ -46,25 +46,36 @@ public class Hash<K> {
         return key.hashCode() % buckets.length;
     }
 
+    private static abstract class Entry<E> {
+        private SimpleEntry<E> next;
 
-    private static class Entry<E> {
-        private final E value;
-        private Entry<E> next;
-
-        private Entry(E value) {
-            this.value = value;
-        }
-
-        public E getValue() {
-            return value;
-        }
-
-        public Entry<E> getNext() {
+        public SimpleEntry<E> getNext() {
             return next;
         }
 
-        public void setNext(Entry<E> next) {
+        public void setNext(SimpleEntry<E> next) {
             this.next = next;
+        }
+
+        public abstract boolean isValueEqualTo(E other);
+    }
+
+
+    private static class NullEntry<E> extends Entry<E> {
+        public boolean isValueEqualTo(E other) {
+            return false;
+        }
+    }
+
+    private static class SimpleEntry<E> extends Entry<E> {
+        private final E value;
+
+        public SimpleEntry(E value) {
+            this.value = value;
+        }
+
+        public boolean isValueEqualTo(E other) {
+            return value.equals(other);
         }
     }
 }
