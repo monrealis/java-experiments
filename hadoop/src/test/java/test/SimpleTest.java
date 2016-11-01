ae5a72a8e2d66e3ea48798848056aa5155f7a1c5
Vytenis Bivainis <monrealis@gmail.com>
2016-11-02 00:43:30 +0200

    Run a "Hello world" with hadoop.

--- hadoop/src/test/java/test/SimpleTest.java
+++ hadoop/src/test/java/test/SimpleTest.java
@@ -2,19 +2,24 @@ package test;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
 
+//http://stackoverflow.com/questions/11342400/how-to-list-all-files-in-a-directory-and-its-subdirectories-in-hadoop-hdfs
 public class SimpleTest {
     @Test
-    @Ignore
     public void run() throws IOException {
         Configuration c = new Configuration();
+        c.set("fs.defaultFS", "hdfs://localhost:8020");
         FileSystem fs = FileSystem.get(c);
-        Path p = new Path("");
-        fs.open(p);
+        RemoteIterator<LocatedFileStatus> it = fs.listFiles(new Path("/"), true);
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
     }
 }
