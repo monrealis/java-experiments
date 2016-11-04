8ad907a530cb1055199eb87c859dc1d1645fefc8
Vytenis Bivainis <monrealis@gmail.com>
2016-11-04 12:16:29 +0200

    Extract methods.

--- nosql/src/test/java/cassandra/CassandraTest.java
+++ nosql/src/test/java/cassandra/CassandraTest.java
@@ -5,30 +5,43 @@ import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class CassandraTest {
+    private Cluster cluster;
+    private Session session;
+
+    @Before
+    public void before() {
+        cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+        session = cluster.connect("demo");
+    }
+
+    @After
+    public void after() {
+        cluster.close();
+    }
+
     @Test
     @Ignore
     public void run() {
-        Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
-        Session session = cluster.connect("demo");
-        session.execute("INSERT INTO users (lastname, age, city, email, firstname) VALUES ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')");
-        ResultSet results = session.execute("SELECT * FROM users WHERE lastname='Jones'");
-        for (Row row : results) {
-            System.out.format("%s %d\n", row.getString("firstname"), row.getInt("age"));
-        }
-        session.execute("update users set age = 36 where lastname = 'Jones'");
-        results = session.execute("select * from users where lastname='Jones'");
-        for (Row row : results) {
-            System.out.format("%s %d\n", row.getString("firstname"), row.getInt("age"));
+        execute("INSERT INTO users (lastname, age, city, email, firstname) VALUES ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')");
+        printResults("SELECT * FROM users WHERE lastname='Jones'");
+        execute("update users set age = 36 where lastname = 'Jones'");
+        printResults("SELECT * FROM users WHERE lastname='Jones'");
+        execute("DELETE FROM users WHERE lastname = 'Jones'");
+        printResults("SELECT * FROM users");
+    }
 
-        }
-        session.execute("DELETE FROM users WHERE lastname = 'Jones'");
-        results = session.execute("SELECT * FROM users");
-        for (Row row : results) {
+    private void printResults(String query) {
+        for (Row row : execute(query))
             System.out.format("%s %d %s %s %s\n", row.getString("lastname"), row.getInt("age"), row.getString("city"), row.getString("email"), row.getString("firstname"));
-        }
+    }
+
+    private ResultSet execute(String query) {
+        return session.execute(query);
     }
 }
