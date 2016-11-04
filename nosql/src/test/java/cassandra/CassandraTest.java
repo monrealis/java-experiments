9168f8efc6079b6e67661f893e1163fa8315ec97
Vytenis Bivainis <monrealis@gmail.com>
2016-11-04 12:09:46 +0200

    Add code from https://academy.datastax.com/resources/getting-started-apache-cassandra-and-java-part-i?unit=2201

--- nosql/src/test/java/cassandra/CassandraTest.java
+++ nosql/src/test/java/cassandra/CassandraTest.java
@@ -2,6 +2,8 @@ package cassandra;
 
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -12,5 +14,21 @@ public class CassandraTest {
     public void run() {
         Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
         Session session = cluster.connect("demo");
+        session.execute("INSERT INTO users (lastname, age, city, email, firstname) VALUES ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')");
+        ResultSet results = session.execute("SELECT * FROM users WHERE lastname='Jones'");
+        for (Row row : results) {
+            System.out.format("%s %d\n", row.getString("firstname"), row.getInt("age"));
+        }
+        session.execute("update users set age = 36 where lastname = 'Jones'");
+        results = session.execute("select * from users where lastname='Jones'");
+        for (Row row : results) {
+            System.out.format("%s %d\n", row.getString("firstname"), row.getInt("age"));
+
+        }
+        session.execute("DELETE FROM users WHERE lastname = 'Jones'");
+        results = session.execute("SELECT * FROM users");
+        for (Row row : results) {
+            System.out.format("%s %d %s %s %s\n", row.getString("lastname"), row.getInt("age"), row.getString("city"), row.getString("email"), row.getString("firstname"));
+        }
     }
 }
