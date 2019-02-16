7bab3f6dc96f894e9090689561c550b2ac37bc86
Vytenis Bivainis <monrealis@gmail.com>
2019-02-16 05:53:50 +0200

    Inspect structured logging

--- logging/src/test/java/log/MyTest.java
+++ logging/src/test/java/log/MyTest.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MarkerFactory;
 
+import net.logstash.logback.argument.StructuredArguments;
+
 public class MyTest {
     Logger log = LoggerFactory.getLogger(MyTest.class);
 
@@ -13,5 +15,23 @@ public class MyTest {
         log.info("my test");
         log.info(MarkerFactory.getMarker("zzz1"), "my test");
         log.info(MarkerFactory.getMarker("zzz2"), "my test");
+        log.info(MarkerFactory.getMarker("zzz2"), "my test", StructuredArguments.keyValue("aaa", "bbb"));
+        log.info("my test", StructuredArguments.keyValue("aaa", "bbb"));
+        log.info("my test: {}", StructuredArguments.keyValue("aaa", "bbb"));
+        log.info("my test", new Z());
+        log.info("my test: {}", new Z());
+        log.info("my test: {}", StructuredArguments.fields(new Z()));
+    }
+
+    public static class Z {
+        private String myField = "my value";
+
+        public String getMyField() {
+            return myField;
+        }
+
+        public void setMyField(String myField) {
+            this.myField = myField;
+        }
     }
 }
