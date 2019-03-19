30d7b0bf2b84d5dcf7e5138c7603fc6704114eed
Vytenis Bivainis <monrealis@gmail.com>
2019-03-20 00:37:33 +0200

    Test jackson serialization to maps and lists

---
+++ jackson/src/test/java/jackson/SerializationTest.java
@@ -0,0 +1,24 @@
+package jackson;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class SerializationTest {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void serialize() throws IOException {
+        DTO dto = new DTO();
+        DTO[] dtos = {dto};
+        System.out.println(mapper.readValue(mapper.writeValueAsString(dto), Object.class));
+        System.out.println(mapper.readValue(mapper.writeValueAsString(dtos), Object.class));
+    }
+
+}
+
+class DTO {
+    public String field = "value";
+}
