f379e0bb08d04dfb397757a5d80d97ef8995e035
Vytenis Bivainis <monrealis@gmail.com>
2024-05-10 12:52:07 +0300

    Remove static imports

--- java8/src/test/java/java8/clock/ClockTest.java
+++ java8/src/test/java/java8/clock/ClockTest.java
@@ -2,7 +2,14 @@ package java8.clock;
 
 import org.junit.Test;
 
-import java.time.*;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,47 +27,47 @@ import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
 import static org.junit.Assert.assertEquals;
 
 public class ClockTest {
-    @Test
-    public void now() {
-        getNows();
-    }
+	@Test
+	public void now() {
+		getNows();
+	}
 
-    private List<Object> getNows() {
-        List<Object> r = new ArrayList<>();
-        r.add(LocalDate.now());
-        r.add(LocalTime.now());
-        r.add(LocalDateTime.now());
-        r.add(Instant.now());
-        r.add(Clock.systemUTC());
-        r.add(ZonedDateTime.now());
-        r.add(OffsetDateTime.now());
-        r.add(OffsetTime.now());
-        return r;
-    }
+	private List<Object> getNows() {
+		List<Object> r = new ArrayList<>();
+		r.add(LocalDate.now());
+		r.add(LocalTime.now());
+		r.add(LocalDateTime.now());
+		r.add(Instant.now());
+		r.add(Clock.systemUTC());
+		r.add(ZonedDateTime.now());
+		r.add(OffsetDateTime.now());
+		r.add(OffsetTime.now());
+		return r;
+	}
 
-    @Test
-    public void format() {
-        assertEquals("20120102", of(2012, 1, 2).format(BASIC_ISO_DATE));
-    }
+	@Test
+	public void format() {
+		assertEquals("20120102", of(2012, 1, 2).format(BASIC_ISO_DATE));
+	}
 
-    @Test
-    public void withGet() {
-        assertEquals(31, of(2012, 1, 2).with(lastDayOfMonth()).get(DAY_OF_MONTH));
-    }
+	@Test
+	public void withGet() {
+		assertEquals(31, of(2012, 1, 2).with(lastDayOfMonth()).get(DAY_OF_MONTH));
+	}
 
-    @Test
-    public void plus() {
-        assertEquals(parse("2012-02-03", ISO_DATE), of(2012, 1, 2).with(MONTH_OF_YEAR, 2).plus(1, DAYS));
-    }
+	@Test
+	public void plus() {
+		assertEquals(parse("2012-02-03", ISO_DATE), of(2012, 1, 2).with(MONTH_OF_YEAR, 2).plus(1, DAYS));
+	}
 
-    @Test
-    public void period() {
-        assertEquals(1, between(parse("2012-02-03"), parse("2012-02-04")).getDays());
-    }
+	@Test
+	public void period() {
+		assertEquals(1, between(parse("2012-02-03"), parse("2012-02-04")).getDays());
+	}
 
-    @Test
-    public void duration() {
-        Instant i = Instant.now();
-        assertEquals(1, between(i, i.plus(1, SECONDS)).getSeconds());
-    }
+	@Test
+	public void duration() {
+		Instant i = Instant.now();
+		assertEquals(1, between(i, i.plus(1, SECONDS)).getSeconds());
+	}
 }
