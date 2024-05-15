fc0117a727751fb2ee2102a212bf42c14133e4eb
Vytenis Bivainis <monrealis@gmail.com>
2024-05-15 15:53:53 +0300

    Separate test in sections

--- java8/src/test/java/java8/futures/CompletableFutureTest.java
+++ java8/src/test/java/java8/futures/CompletableFutureTest.java
@@ -8,39 +8,41 @@ import java.util.concurrent.ExecutionException;
 import static org.junit.Assert.assertEquals;
 
 public class CompletableFutureTest {
-    public static final Integer TWO = 2;
-    public static final Integer THREE = 3;
-    public static final Integer FOUR = 4;
+	private static final Integer TWO = 2;
+	private static final Integer THREE = 3;
+	private static final Integer FOUR = 4;
 
-    @Test
-    public void get() throws ExecutionException, InterruptedException {
-        CompletableFuture<Integer> cf = supplyTwoAsync();
-        assertEquals(TWO, cf.get());
-    }
+	@Test
+	public void get() throws ExecutionException, InterruptedException {
+		CompletableFuture<Integer> cf = supplyTwoAsync();
 
-    private CompletableFuture<Integer> supplyTwoAsync() {
-        return CompletableFuture.supplyAsync(() -> 2);
-    }
+		assertEquals(TWO, cf.get());
+	}
 
-    @Test
-    public void join() {
-        CompletableFuture<Integer> cf = supplyTwoAsync();
-        assertEquals(TWO, cf.join());
-    }
+	private CompletableFuture<Integer> supplyTwoAsync() {
+		return CompletableFuture.supplyAsync(() -> 2);
+	}
 
-    @Test
-    public void thenCombine() {
-        assertEquals(FOUR, supplyTwoAsync().thenCombine(supplyTwoAsync(), (a, b) -> a + b).join());
-        assertEquals(FOUR, supplyTwoAsync().thenCombineAsync(supplyTwoAsync(), (a, b) -> a + b).join());
-    }
+	@Test
+	public void join() {
+		CompletableFuture<Integer> cf = supplyTwoAsync();
 
-    @Test
-    public void thenCompose() {
-        assertEquals(THREE, supplyTwoAsync().thenCompose(this::addOneAsync).join());
-        assertEquals(THREE, supplyTwoAsync().thenComposeAsync(this::addOneAsync).join());
-    }
+		assertEquals(TWO, cf.join());
+	}
 
-    private CompletableFuture<Integer> addOneAsync(int number) {
-        return CompletableFuture.supplyAsync(() -> number + 1);
-    }
+	@Test
+	public void thenCombine() {
+		assertEquals(FOUR, supplyTwoAsync().thenCombine(supplyTwoAsync(), (a, b) -> a + b).join());
+		assertEquals(FOUR, supplyTwoAsync().thenCombineAsync(supplyTwoAsync(), (a, b) -> a + b).join());
+	}
+
+	@Test
+	public void thenCompose() {
+		assertEquals(THREE, supplyTwoAsync().thenCompose(this::addOneAsync).join());
+		assertEquals(THREE, supplyTwoAsync().thenComposeAsync(this::addOneAsync).join());
+	}
+
+	private CompletableFuture<Integer> addOneAsync(int number) {
+		return CompletableFuture.supplyAsync(() -> number + 1);
+	}
 }
