d750d98915e7932fca96c5c69e7c9d51c7825ae3
Vytenis Bivainis <monrealis@gmail.com>
2024-05-16 10:14:19 +0300

    Remove unnecessary keywords

--- java8/src/test/java/java8/interfaces/InterfaceMethodsTest.java
+++ java8/src/test/java/java8/interfaces/InterfaceMethodsTest.java
@@ -5,26 +5,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class InterfaceMethodsTest {
-    private I i = new I() {
-    };
+	I i = new I() {
+	};
 
-    @Test
-    public void five() {
-        assertEquals(5, i.five());
-    }
+	@Test
+	public void five() {
+		assertEquals(5, i.five());
+	}
 
-    @Test
-    public void twoTimesFive() {
-        assertEquals(10, I.twoTimesFive());
-    }
+	@Test
+	public void twoTimesFive() {
+		assertEquals(10, I.twoTimesFive());
+	}
 
-    public static interface I {
-        public static int twoTimesFive() {
-            return 5 * 2;
-        }
+	interface I {
+		public static int twoTimesFive() {
+			return 5 * 2;
+		}
 
-        default int five() {
-            return 5;
-        }
-    }
+		default int five() {
+			return 5;
+		}
+	}
 }
