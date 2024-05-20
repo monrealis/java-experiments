ee97e038d18e2915525e7d6442ce0878b16bc715
Vytenis Bivainis <monrealis@gmail.com>
2024-05-20 11:23:55 +0300

    Use static imports

--- java8/src/test/java/java8/mrs/SortingTest.java
+++ java8/src/test/java/java8/mrs/SortingTest.java
@@ -3,33 +3,35 @@ package java8.mrs;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.shuffle;
 import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.rangeClosed;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class SortingTest {
-    private List<Integer> ints = IntStream.rangeClosed(1, 10).boxed().collect(Collectors.toList());
+    private List<Integer> ints = rangeClosed(1, 10).boxed().collect(toList());
 
     @Before
     public void before() {
-        Collections.shuffle(ints);
+        shuffle(ints);
     }
 
     @Test
     public void usingMethodReferences_sorted() {
         ints.sort(comparing(Object::toString));
+
         assertSorted();
     }
 
     @Test
     public void usingLambdas_sorted() {
         ints.sort(comparing(a -> a.toString()));
+
         assertSorted();
     }
 
