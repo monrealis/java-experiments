097a4654c3c806f92b195c1678b88c392880dcae
Vytenis Bivainis <monrealis@gmail.com>
2024-05-28 10:05:40 +0300

    Rename getters, remove brackets, use static imports

--- java8/src/test/java/java8/streams/StreamsTest.java
+++ java8/src/test/java/java8/streams/StreamsTest.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
 
 public class StreamsTest {
@@ -19,19 +19,18 @@ public class StreamsTest {
 
     @Test
     public void toList_collectsListEqualToOriginal() {
-        assertEquals(stream.collect(Collectors.toList()), list);
+        assertEquals(stream.collect(toList()), list);
     }
 
     @Test
     public void flatMap_flattensItems() {
-        assertEquals(stream.flatMap(Collection::stream).collect(Collectors.toList()), getFlatList());
+        assertEquals(stream.flatMap(Collection::stream).collect(toList()), flatList());
     }
 
-    private List<String> getFlatList() {
+    private List<String> flatList() {
         List<String> expected = new ArrayList<>();
-        for (List<String> l : list) {
+        for (List<String> l : list)
             expected.addAll(l);
-        }
         return expected;
     }
 }
