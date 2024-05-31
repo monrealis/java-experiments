7ce95293566823a2f08d59e42e600f49978c694a
Vytenis Bivainis <monrealis@gmail.com>
2024-05-31 10:22:51 +0300

    Add accessors annotation

--- lombok/src/main/java/eu/vytenis/lombok/Bean.java
+++ lombok/src/main/java/eu/vytenis/lombok/Bean.java
@@ -2,9 +2,11 @@ package eu.vytenis.lombok;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 public class Bean {
     @Getter
     @Setter
+    @Accessors(fluent = false)
     private String property;
 }
