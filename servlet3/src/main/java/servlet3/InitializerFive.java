cc714b81e659a5d4b5fa86f110b74e215cb6182f
Vytenis Bivainis <monrealis@gmail.com>
2024-12-23 13:27:49 +0200

    Use static imports

--- servlet3/src/main/java/servlet3/InitializerFive.java
+++ servlet3/src/main/java/servlet3/InitializerFive.java
@@ -1,16 +1,17 @@
 package servlet3;
 
 import javax.servlet.Filter;
+import static java.lang.System.out;
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HandlesTypes;
 import java.util.Set;
 
-@HandlesTypes({ServletOne.class, Filter.class})
+@HandlesTypes({ ServletOne.class, Filter.class })
 public class InitializerFive implements ServletContainerInitializer {
     @Override
     public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
-        System.out.println(set);
+        out.println(set);
     }
 }
