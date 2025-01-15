6e95a1799557b49b189909174a90d457921771cd
Vytenis Bivainis <monrealis@gmail.com>
2025-01-15 22:25:45 +0200

    Undo test question from LinkedIn

--- servlet3/src/main/java/servlet3/Main.java
+++
@@ -1,8 +0,0 @@
-package servlet3;
-
-public class Main {
-    public static void main(String[] args) {
-       System.out.print(10 + 20 + "FTA" + " ");
-       System.out.print("FTA" + 10 + 20);
-    }
-}
