97c708f6c8e4856e51b8413c0d77410020e4d1a6
Vytenis Bivainis <monrealis@gmail.com>
2025-01-15 22:23:51 +0200

    Test question from LinkedIn

---
+++ servlet3/src/main/java/servlet3/Main.java
@@ -0,0 +1,8 @@
+package servlet3;
+
+public class Main {
+    public static void main(String[] args) {
+       System.out.print(10 + 20 + "FTA" + " ");
+       System.out.print("FTA" + 10 + 20);
+    }
+}
