0e06697bf39c664fca89e645888146bcf1f59750
Vytenis Bivainis <monrealis@gmail.com>
2025-01-18 13:35:37 +0200

    Check for palyndrome

--- servlet3/src/main/java/servlet3/Main.java
+++ servlet3/src/main/java/servlet3/Main.java
@@ -2,14 +2,8 @@ package servlet3;
 
 public class Main {
     public static void main(String[] args) {
-        int s = redo(200, 3);
-        System.out.print(s);
-    }
-
-    private static int redo(int i, int j) {
-        if (i == 0)
-            return 0;
-        else
-            return redo(i / j, j) + 1;
+        String string = "Madam";
+        StringBuilder s = new StringBuilder(string);
+        System.out.println(s.reverse().toString().equals(string));
     }
 }
