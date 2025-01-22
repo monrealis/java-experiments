6486da5b6d6131de334ccbd5f003ec039db0680b
Vytenis Bivainis <monrealis@gmail.com>
2025-01-22 14:11:15 +0200

    Add various cases for fibonacci test

--- servlet3/src/test/java/servlet3/VariousTests.java
+++ servlet3/src/test/java/servlet3/VariousTests.java
@@ -2,6 +2,7 @@ package servlet3;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 
@@ -35,14 +36,28 @@ public class VariousTests {
 
     @Test
     public void fibonacci() {
+        assertEquals(4181, fibonaci(20));
+        assertEquals(34, fibonaci(10));
+        assertEquals(0, fibonaci(1));
+        assertEquals(1, fibonaci(2));
+        assertEquals(1, fibonaci(3));
+        assertEquals(2, fibonaci(4));
+        assertEquals(3, fibonaci(5));
+        assertThrows(IllegalArgumentException.class, () -> fibonaci(0));
+    }
+
+    private int fibonaci(int n) {
+        if (n < 1)
+            throw new IllegalArgumentException();
+        if (n == 1)
+            return 0;
         int i = 0;
         int j = 1;
-        for (int n = 2; n < 20; ++n) {
+        for (int ii = 2; ii < n; ++ii) {
             int l = i + j;
             i = j;
             j = l;
         }
-
-        assertEquals(j, 4181);
+        return j;
     }
 }
