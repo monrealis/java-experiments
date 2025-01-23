e9caf4b858afbadb4f673fcbbbc87b9e661bdbfd
Vytenis Bivainis <monrealis@gmail.com>
2025-01-23 22:31:04 +0200

    Bump to jUnit 5.9.13

--- servlet3/src/test/java/servlet3/VariousTests.java
+++ servlet3/src/test/java/servlet3/VariousTests.java
@@ -1,10 +1,10 @@
 package servlet3;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class VariousTests {
     @Test
