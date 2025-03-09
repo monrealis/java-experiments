d619cec2427700273415fed9879964263486ec7b
Vytenis Bivainis <monrealis@gmail.com>
2025-03-09 12:22:06 +0200

    Pass the tests

--- servlet3/src/test/java/servlet3/VariousTests.java
+++ servlet3/src/test/java/servlet3/VariousTests.java
@@ -227,7 +227,7 @@ public class VariousTests {
     @ParameterizedTest
     @CsvSource({ "0,10,0,2", "1,10,1,2", "2,10,10,2", "2,10,2,3", "3,10,11,2", "3,10,10,3", "4,10,100,2", "5,10,101,2",
             "5,5,5,10", "10,10,(10),11", "11,10,10,11", "12,10,11,11", "24,10,22,11", "120,10,(10)(10),11", "30,4,22,5",
-            "31,4,23,5", "(10),11,11,10" })
+            "31,4,23,5", "(10),11,10,10", "(10)(10),11,1010,10" })
     public void changeBaseToN(String decimal, int fromBase, String baseN, int toBase) {
         int d = parse(decimal, fromBase);
         int bN;
@@ -245,20 +245,17 @@ public class VariousTests {
     private int parse(String number, int fromBase) {
         int sum = 0;
         char[] chars = number.toCharArray();
-        boolean inside = false;
         boolean opened = false;
         for (int i = 0; i < chars.length; ++i) {
-            inside = true;
             if (chars[i] == '(') {
                 assertEquals(opened, false);
                 opened = true;
-
             } else if (chars[i] == ')') {
                 assertEquals(opened, true);
                 opened = false;
             } else if (opened) {
                 int cc = chars[i] - '0';
-                sum *= fromBase;
+                sum *= 10;
                 sum += cc;
             } else {
                 int cc = chars[i] - '0';
