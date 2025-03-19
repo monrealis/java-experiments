90b81e09a6ec2d538c87266a2fdee5ef8f831f2f
Vytenis Bivainis <monrealis@gmail.com>
2025-03-19 13:40:37 +0200

    Fix the test

--- servlet3/src/test/java/servlet3/ChangeBaseTest.java
+++ servlet3/src/test/java/servlet3/ChangeBaseTest.java
@@ -16,7 +16,7 @@ public class ChangeBaseTest {
     @ParameterizedTest
     @CsvSource({ "0,10,0,2", "1,10,1,2", "2,10,10,2", "2,10,2,3", "3,10,11,2", "3,10,10,3", "4,10,100,2", "5,10,101,2",
             "5,5,5,10", "10,10,(10),11", "11,10,10,11", "12,10,11,11", "24,10,22,11", "120,10,(10)(10),11", "30,4,22,5",
-            "31,4,23,5", "(10),11,10,10", "(10)(10),11,120,10", "(11),12,11,10", "(11)(10)0,12,1704,10", "(9),2,9,10",
+            "31,4,23,5", "(10),11,10,10", "(10)(10),11,120,10", "(11),12,11,10", "(11)(10)0,12,1704,10", "(1)(0)01,2,9,10",
             "1(),10,10,10", "(),10,0,1" })
     public void changeBaseToN(String decimal, int fromBase, String baseN, int toBase) {
         int d = new Parser(decimal, fromBase).parse();
@@ -52,9 +52,11 @@ public class ChangeBaseTest {
                 else if (chars[i] == ')')
                     close();
                 else if (opened) {
-                    int charInParen = chars[i] - '0';
+                    int cc = chars[i] - '0';
+                    if (cc < 0 || cc > fromBase)
+                        throw new IllegalArgumentException();
                     sumInParen *= 10;
-                    sumInParen += charInParen;
+                    sumInParen += cc;
                 } else {
                     int cc = chars[i] - '0';
                     if (cc < 0 || cc > fromBase)
