0010632ba12411b7c41304d6c5a3a5f452d1f145
Vytenis Bivainis <monrealis@gmail.com>
2025-04-30 10:23:55 +0300

    Sort using bubble sort

--- servlet3/src/test/java/servlet3/VariousTests.java
+++ servlet3/src/test/java/servlet3/VariousTests.java
@@ -634,4 +634,22 @@ public class VariousTests {
 
         assertEquals(sum, expectedSum);
     }
+
+    @Test
+    public void bubbleSort() {
+        int[] arr = { 1, 5, 3, 1, 2 };
+        for (int i = 0; i < arr.length; i++)
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+
+        assertEquals(1, arr[0]);
+        assertEquals(1, arr[1]);
+        assertEquals(2, arr[2]);
+        assertEquals(3, arr[3]);
+        assertEquals(5, arr[4]);
+    }
 }
