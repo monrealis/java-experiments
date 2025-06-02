c6e2fd34c00d68ee31ef4cfff81d635b95c38da9
Vytenis Bivainis <monrealis@gmail.com>
2025-06-02 15:14:56 +0300

    Add tests for trigonometric functions

---
+++ servlet3/src/test/java/servlet3/TrigonometricFunctionsTest.java
@@ -0,0 +1,30 @@
+package servlet3;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class TrigonometricFunctionsTest {
+    @ParameterizedTest
+    @CsvSource({ "0,0"/* , "1,0.6" */ })
+    public void sinX(double x, double expectedResult) {
+        double sin = 0;
+        int inverse = 0;
+        for (int i = 1; i < 6; i += 2) {
+            double added = pow(x, i) / factorial(i);
+            sin += added;
+            inverse = -inverse;
+        }
+        assertTrue(abs(sin - expectedResult) < 0.1, sin + " " + expectedResult);
+    }
+
+    private double factorial(int n) {
+        double f = 1;
+        for (int i = 1; i < n; ++i)
+            f *= i + 1;
+        return f;
+    }
+}
