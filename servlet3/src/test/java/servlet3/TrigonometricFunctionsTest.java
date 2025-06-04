64d3d42987110174f56c3f7be9e50a6a402d6f9c
Vytenis Bivainis <monrealis@gmail.com>
2025-06-04 13:21:21 +0300

    Improve precision

--- servlet3/src/test/java/servlet3/TrigonometricFunctionsTest.java
+++ servlet3/src/test/java/servlet3/TrigonometricFunctionsTest.java
@@ -1,7 +1,6 @@
 package servlet3;
 
 import static java.lang.Math.abs;
-import static java.lang.Math.pow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,17 +8,20 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class TrigonometricFunctionsTest {
     @ParameterizedTest
-    @CsvSource({ "0,0", "1,0.841", "-1,-0.841" })
+    @CsvSource({ "0,0", "1,0.841471", "-1,-0.841471" })
     public void sinX(double x, double expectedResult) {
         double sin = 0;
         double factorial = 1;
+
         for (int i = 0; i < 10; ++i) {
             double f = factorial(2 * i + 1);
-            double added = (i % 2 == 0 ? 1 : -1) * pow(x, 2 * i + 1) / f;
-            factorial *= (factorial + 2 * i + 1) * (factorial + 2 * i + 2);
+            double added = (i % 2 == 0 ? 1 : -1) * x / f;
+            x = x * x * x;
+            double factorial1 = factorial * (factorial + 2 * i + 1) * (factorial + 2 * i + 2);
+            factorial = factorial1;
             sin += added;
         }
-        assertTrue(abs(sin - expectedResult) < 0.001, sin + " " + expectedResult);
+        assertTrue(abs(sin - expectedResult) < 0.0001, sin + " " + expectedResult);
     }
 
     private double factorial(int n) {
