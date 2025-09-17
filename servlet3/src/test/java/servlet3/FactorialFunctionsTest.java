adbe605733f5aec9e572d106c29bbf62f99daca4
Vytenis Bivainis <monrealis@gmail.com>
2025-09-17 09:24:10 +0300

    Add a function parameter to arcTgOrCtg

--- servlet3/src/test/java/servlet3/FactorialFunctionsTest.java
+++ servlet3/src/test/java/servlet3/FactorialFunctionsTest.java
@@ -241,16 +241,16 @@ public class FactorialFunctionsTest {
     @ParameterizedTest(name = "arcctgx({0}) ≈ {1} (tolerance: {2})")
     @CsvSource({ "1,0.7853,0.01", "0,0,0.001", "-1,-0.7853,0.01", "0.5,.463,0.001", "-0.5,-0.463,0.001" })
     public void arcctgx(double x, double expected, double tolerance) {
-        arcTgOrCtg(x, expected, tolerance);
+        arcTgOrCtg(x, expected, tolerance, "arcctg");
     }
 
     @ParameterizedTest(name = "arctgx({0}) ≈ {1} (tolerance: {2})")
     @CsvSource({ "1,0.7853,0.01", "0,0,0.001", "-1,-0.7853,0.01", "0.5,.463,0.001", "-0.5,-0.463,0.001" })
     public void arctgx(double x, double expected, double tolerance) {
-        arcTgOrCtg(x, expected, tolerance);
+        arcTgOrCtg(x, expected, tolerance, "arctg");
     }
 
-    private void arcTgOrCtg(double x, double expected, double tolerance) {
+    private void arcTgOrCtg(double x, double expected, double tolerance, String function) {
         double arctg = 0;
         double factorialNom = 1;
         double factorialDen = 1;
@@ -267,7 +267,7 @@ public class FactorialFunctionsTest {
 
         double error = abs(arctg - expected);
 
-        String msg = format("arctg(%f) → approx %.6f, error = %.6f exceeds tolerance %.6f", x, expected, error,
+        String msg = format("%s(%f) → approx %.6f, error = %.6f exceeds tolerance %.6f", function, x, expected, error,
                 tolerance);
         assertTrue(error < tolerance, msg);
     }
