89eef8e74f74197d75309f1e338b48c84f4fa22b
Vytenis Bivainis <monrealis@gmail.com>
2025-09-29 10:45:12 +0300

    Add a failing test

--- servlet3/src/test/java/servlet3/PI.java
+++ servlet3/src/test/java/servlet3/PI.java
@@ -1,10 +1,22 @@
 package servlet3;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class PI {
     private static double pi = Math.PI;
 
-    public void wallis(double tolerance) {
-
+    @Test
+    public void wallis() {
+        double sum = 0;
+        for (int i = 1; i < 3; i++) {
+            double nom = 2 * i * 2 * i;
+            double den = (2 * i - 1) * (2 * i + 1);
+            double added = 1.0 * nom / den;
+            sum += added;
+        }
+        assertEquals(pi, 2 * sum, 0.001);
     }
 
     public void stirling(double tolerance) {
