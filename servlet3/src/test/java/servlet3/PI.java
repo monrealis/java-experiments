38df26f253298c221561b74149a91b29b0edea1a
Vytenis Bivainis <monrealis@gmail.com>
2025-10-01 09:57:01 +0300

    Leave a failing test

--- servlet3/src/test/java/servlet3/PI.java
+++ servlet3/src/test/java/servlet3/PI.java
@@ -12,13 +12,13 @@ public class PI {
     @CsvSource({ "0.001" })
     public void wallis(double tolerance) {
         double sum = 0;
-        for (int i = 1; i < 3; i++) {
-            double nom = 2 * i * 2 * i;
-            double den = (2 * i - 1) * (2 * i + 1);
+        for (int i = 1; i <= 30; i++) {
+            double nom = 4 * i * i;
+            double den = 4 * i * i - 1;
             double added = 1.0 * nom / den;
             sum += added;
         }
-        assertEquals(pi, 2 * sum, tolerance);
+        assertEquals(pi / 2, sum, tolerance);
     }
 
     public void stirling(double tolerance) {
