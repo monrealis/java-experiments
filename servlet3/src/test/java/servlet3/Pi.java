95a0e9009b1204becc7640527ff1d655649bf5d0
Vytenis Bivainis <monrealis@gmail.com>
2025-10-08 14:31:47 +0300

    Add WIP

--- servlet3/src/test/java/servlet3/Pi.java
+++ servlet3/src/test/java/servlet3/Pi.java
@@ -1,6 +1,7 @@
 package servlet3;
 
 import static java.lang.Math.PI;
+import static java.lang.Math.log;
 import static java.lang.Math.pow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static servlet3.Factorials.factorial;
@@ -50,14 +51,19 @@ public class Pi {
 
     @ParameterizedTest
     @CsvSource("0.05")
-    @Disabled
     public void stirlingApproximation(double tolerance) {
-        int n = 500;
-        double nominator = 1;
-        double denumerator = 1;
-        double result = nominator / denumerator;
-
-        assertEquals(pi, result * 2, tolerance);
+        int n = 100;
+        double a = log(n);
+        double c = log(2 * n + 1);
+        double d = log(2);
+        double b = log(2 * pi * n);
+        double e = log(2 * pi * (2 * n + 1));
+        double part1 = 2 * n * a;
+        double part2 = -2 * n;
+        double part3 = b;
+        double part4 = (2 * n + 1) * d;
+        double part5 = -(2 * n + 1) * c;
+        assertEquals(pi, pi, tolerance);
     }
 
     public void ramanujan(double tolerance) {
