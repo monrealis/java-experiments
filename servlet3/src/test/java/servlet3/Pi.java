a2e6a1247307e4c0437f07f10ed8ea8b9c50a26a
Vytenis Bivainis <monrealis@gmail.com>
2025-10-09 13:28:59 +0300

    Make the Ramanujan test pass

--- servlet3/src/test/java/servlet3/Pi.java
+++ servlet3/src/test/java/servlet3/Pi.java
@@ -1,8 +1,8 @@
 package servlet3;
 
 import static java.lang.Math.PI;
-import static java.lang.Math.log;
 import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static servlet3.Factorials.factorial;
 
@@ -51,22 +51,18 @@ public class Pi {
 
     @ParameterizedTest
     @CsvSource("0.05")
-    public void stirlingApproximation(double tolerance) {
-        int n = 100;
-        double a = log(n);
-        double c = log(2 * n + 1);
-        double d = log(2);
-        double b = log(2 * pi * n);
-        double e = log(2 * pi * (2 * n + 1));
-        double part1 = 2 * n * a;
-        double part2 = -2 * n;
-        double part3 = b;
-        double part4 = (2 * n + 1) * d;
-        double part5 = -(2 * n + 1) * c;
-        assertEquals(pi, pi, tolerance);
-    }
-
     public void ramanujan(double tolerance) {
-
+        double result = 0;
+        double factor = 2 * sqrt(2) / 9801;
+        for (int k = 0; k < 5; ++k) {
+            double f = factorial(k);
+            double numerator = factorial(4 * k) * (1103 + 26390 * k);
+            double denominator = f * f * f * f * pow(396, 4 * k);
+            double added = numerator / denominator;
+            result += added;
+        }
+        double temp = result * factor;
+        double temp1 = 1 / temp;
+        assertEquals(pi, temp1, tolerance);
     }
 }
