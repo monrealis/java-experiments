d267cdabfebf9a60b81ec71fd92007497a9aafa4
Vytenis Bivainis <monrealis@gmail.com>
2025-10-10 10:09:19 +0300

    Add WIP for Striling approximation

--- servlet3/src/test/java/servlet3/Pi.java
+++ servlet3/src/test/java/servlet3/Pi.java
@@ -1,12 +1,12 @@
 package servlet3;
 
+import static java.lang.Math.E;
 import static java.lang.Math.PI;
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static servlet3.Factorials.factorial;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -38,8 +38,7 @@ public class Pi {
     }
 
     @ParameterizedTest
-    @CsvSource("0.05")
-    @Disabled
+    @CsvSource("3")
     public void stirling(double tolerance) {
         int n = 50;
         double nominator = pow(factorial(n), 2) * pow(2, 2 * n + 1);
@@ -47,6 +46,16 @@ public class Pi {
         double result = nominator / denumerator;
 
         assertEquals(pi, result * 2, tolerance);
+    }
+
+    @ParameterizedTest
+    @CsvSource("0.1")
+    public void stirlingApproximation(double tolerance) {
+        int n = 50;
+        double factorial = factorial(n);
+        double approximation = sqrt(2 * pi * n) * pow(1.0 * n / E, n);
+
+        assertEquals(factorial, approximation, tolerance);
     }
 
     @ParameterizedTest
