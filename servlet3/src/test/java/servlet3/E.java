bd69a55c92e74b4b8b9ca150fd645d9349174dd6
Vytenis Bivainis <monrealis@gmail.com>
2025-11-19 10:38:54 +0200

    Pass e test according to its definition

--- servlet3/src/test/java/servlet3/E.java
+++ servlet3/src/test/java/servlet3/E.java
@@ -1,5 +1,8 @@
 package servlet3;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static servlet3.Powers.power;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -7,6 +10,10 @@ public class E {
     @ParameterizedTest
     @CsvSource({ "0.001" })
     public void e(double tolerance) {
+        int pow = 10000;
 
+        double result = power(1 + 1.0 / pow, pow);
+
+        assertEquals(Math.E, result, tolerance);
     }
 }
