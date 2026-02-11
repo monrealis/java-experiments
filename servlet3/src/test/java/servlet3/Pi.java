c3828add4977f0794c607048b4ea99a65bb8ce15
Vytenis Bivainis <monrealis@gmail.com>
2026-02-11 08:11:09 +0200

    Increase the expected precision

--- servlet3/src/test/java/servlet3/Pi.java
+++ servlet3/src/test/java/servlet3/Pi.java
@@ -34,7 +34,7 @@ public class Pi {
     }
 
     @ParameterizedTest
-    @CsvSource({ "0.001" })
+    @CsvSource({ "0.0009" })
     public void wallis(double tolerance) {
         double result = 0;
         for (int i = 1; i <= 1000; i++) {
