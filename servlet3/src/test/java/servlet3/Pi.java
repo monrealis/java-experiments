b1cb2320931e2aa58abc1ccf71e5a2f8645bbfb0
Vytenis Bivainis <monrealis@gmail.com>
2026-02-18 15:38:55 +0200

    Increase the expected precision

--- servlet3/src/test/java/servlet3/Pi.java
+++ servlet3/src/test/java/servlet3/Pi.java
@@ -82,7 +82,7 @@ public class Pi {
     }
 
     @ParameterizedTest
-    @CsvSource("0.05")
+    @CsvSource("0.04")
     public void ramanujan(double tolerance) {
         double sum = 0;
         double factor = 2 * sqrt(2) / (99 * 99);
