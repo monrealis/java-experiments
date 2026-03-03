b083a81144e5c140551499682e68ad9787238d96
Vytenis Bivainis <monrealis@gmail.com>
2026-03-04 00:10:57 +0200

    Use static imports

--- servlet3/src/test/java/servlet3/FactorialsTest.java
+++ servlet3/src/test/java/servlet3/FactorialsTest.java
@@ -1,6 +1,7 @@
 package servlet3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static servlet3.Factorials.factorial;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -8,13 +9,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class FactorialsTest {
     @ParameterizedTest
     @CsvSource({ "0,1", "1,1", "2,2", "3,6" })
-    public void factorial(int n, int expectedResult) {
-        assertEquals(Factorials.factorial(n), expectedResult);
+    public void factorials(int n, int expectedResult) {
+        assertEquals(factorial(n), expectedResult);
     }
 
     @ParameterizedTest
     @CsvSource({ "6,3,4,24", "2,2,4,24", "1,1,4,24", "1,0,4,24" })
-    public void factorial(int initial, int fromN, int n, int expectedResult) {
-        assertEquals(Factorials.factorial(initial, fromN, n), expectedResult);
+    public void factorials(int initial, int fromN, int n, int expectedResult) {
+        assertEquals(factorial(initial, fromN, n), expectedResult);
     }
 }
