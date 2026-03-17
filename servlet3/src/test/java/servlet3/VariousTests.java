d0958d514e97478fd56f09eb29df9f2297d11a43
Vytenis Bivainis <monrealis@gmail.com>
2026-03-17 08:43:22 +0200

    Add a test case

--- servlet3/src/test/java/servlet3/VariousTests.java
+++ servlet3/src/test/java/servlet3/VariousTests.java
@@ -100,7 +100,7 @@ public class VariousTests {
     @ParameterizedTest
     @CsvSource({ "true,2", "true,3", "false,4", "true,5", "false,9", "false,16", "false,25", "false,49", "false,64",
             "false,81", "false,99", "true,97", "false,98", "false,99", "false,100", "true,101", "true,103", "true,109",
-            "true,113", "false,119", "false,121", "true,127", "false,129", "true,131", "false,133" })
+            "true,113", "false,119", "false,121", "true,127", "false,129", "true,131", "false,133", "true,137" })
     public void primeNumbers(boolean expectedResult, int n) {
         assertEquals(expectedResult, isPrimeNumber(n));
     }
