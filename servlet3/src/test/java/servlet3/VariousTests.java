8184a702673658406115402c21c007821348c1d5
Vytenis Bivainis <monrealis@gmail.com>
2026-04-25 23:36:28 +0300

    Add a test case

--- servlet3/src/test/java/servlet3/VariousTests.java
+++ servlet3/src/test/java/servlet3/VariousTests.java
@@ -104,7 +104,7 @@ public class VariousTests {
             "true,139", "true,149", "true,151", "false,153", "true,157", "false,161", "true,163", "false,195",
             "true,167", "false,169", "true,173", "true,179", "true,181", "false,183", "false,187", "true,191",
             "true,193", "true,197", "true,199", "false,201", "false,203", "false,209", "true,211", "false,217",
-            "false,221", "true,223", "true,227", "true,229" })
+            "false,221", "true,223", "true,227", "true,229", "false,231" })
     public void primeNumbers(boolean expectedResult, int n) {
         assertEquals(expectedResult, isPrimeNumber(n));
     }
